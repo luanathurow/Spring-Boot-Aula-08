c99218b765285d34b700019f5bf111a982d91263
Luana Thurow <118767921+luanathurow@users.noreply.github.com>
2025-04-10 23:12:55 +0000

    update endpoint

--- exemplo/src/main/java/br/pucrs/luana/thurow/exemplo/ExemploController.java
+++ exemplo/src/main/java/br/pucrs/luana/thurow/exemplo/ExemploController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,16 +21,35 @@ public class ExemploController {
     List<Livro> livros = new ArrayList<>();
 
     public ExemploController() {
-        livros.add(new Livro(1, "O Senhor dos Anéis", "J.R.R. Tolkien", 200));
-        livros.add(new Livro(2, "Dom Casmurro", "Machado de Assis", 1999));
-        livros.add(new Livro(3, "Harry Potter e a Pedra Filosofal", "J.K. Rowling", 1983));
+        livros.add(new Livro(1, "Jogos Vorazes", "Luana Thurow", 2000));
+        livros.add(new Livro(2, "Turma da Monica", "Mauricio de Souza", 1950));
+        livros.add(new Livro(3, "Harry Potter", "J.K. Rowling", 1983));
     }
 
-    @PostMapping("/cadastro")
-    public String cadastroLivro(@RequestBody Livro livro) {
-        return "Livro cadastrado com sucesso: " + livro.getTitulo();
-    }    
+    @GetMapping("/livrosautor")
+    public List<Livro> getLivrosDoAutor(@RequestParam(value = "autor") String autor) {
+        return livros.stream()
+            .filter(livro->livro.getAutor().equals(autor))
+            .toList();
+    }
 
+    @PostMapping("/novolivro")
+    public boolean cadastraLivroNovo(@RequestBody final Livro livro) {
+    livros.add(livro);
+    return true;
+    }
+
+    @GetMapping("/livrotitulo/{titulo}")
+    public ResponseEntity<Livro> getLivroTitulo(@PathVariable("titulo") String titulo) {
+    Livro resp = livros.stream()
+    .filter(livro->livro.getTitulo().equals(titulo))
+    .findFirst()
+    .orElse(null); 
+    return ResponseEntity
+    .status(HttpStatus.OK)
+    .body(resp);
+    }
+    
     @GetMapping("/")
     public String getMensagemInicial() {
         return "Aplicação Spring-Boot funcionando!";
